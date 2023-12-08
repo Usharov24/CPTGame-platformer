fe7e74012a7a268d952483a84900e6bcabc6224a
Gregory-Usharov <gregory.usharov24@ycdsbk12.ca>
2023-12-08 15:29:30 -0500

    Checking for pulls
    Seeing if collabs can see edits and if they can pull the updated version

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String args[]){
         System.out.println("pain");
         System.out.println("pain2");
+        System.out.println("watig for albert");
     }
     
 }
