8a3983887080b8d69b2536588940c08f5d4d2207
Gregory-Usharov <gregory.usharov24@ycdsbk12.ca>
2023-12-17 22:21:49 -0500

    Created class for homing bullets
    the class for homing bullets will function just as the bullets class but will have a second target destination.

---
+++ objects/HomingBullet.java
@@ -0,0 +1,35 @@
+package objects;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.LinkedList;
+
+import framework.ObjectHandler;
+import framework.ObjectId;
+
+public class HomingBullet extends GameObject {
+
+    private ObjectHandler handler;
+
+    
+    public HomingBullet(float fltX, float fltY, float fltVelX, float fltVelY, float fltWidth, float fltHeight, ObjectId id, ObjectHandler handler) {
+        super(fltX, fltY, fltWidth, fltHeight, id);
+        this.fltVelX = fltVelX;
+        this.fltVelY = fltVelY;
+        this.handler = handler;
+    }
+        
+    public void update(LinkedList<GameObject> objectList) {
+        
+        fltX += fltVelX;
+        fltY += fltVelY;
+
+        if(fltX > 1280 || fltX < 0 || fltY > 720 || fltY < 0){
+            handler.removeObject(this);
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect((int)fltX, (int)fltY, (int)fltWidth, (int)fltHeight);
+    }
+}
